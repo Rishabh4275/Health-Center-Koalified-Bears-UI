dd296d02ef196f1969e7fbbd322f962182df7001
Mudit Garg <mudgarg@Mudits-MacBook-Pro.local>
2019-10-31 15:06:42 -0400

    Appointment UI

--- js/appointment.js
+++ js/appointment.js
@@ -1,49 +1,39 @@
 var button=document.getElementById('appointment-submit');
 
 button.onclick = function() {
-	var name = document.getElementById('appointment_name');
+	var name = document.getElementById('appointment_name').value;
 	var dept = document.getElementById('appointment_department');
-	var email = document.getElementById('appointment_email');
-	var mobile = document.getElementById('phone');
-	var time = document.getElementById('appointment_time');
-	var date = document.getElementById('appointment_date');
+	var deptId = dept.options[dept.selectedIndex].value;
+	var email = document.getElementById('appointment_email').value;
+	var mobile = document.getElementById('phone').value;
+	var time = document.getElementById('appointment_time').value;
+	var dt = document.getElementById('appointment_date').value;
+	var date=Date.parse(dt);
+	console.log(date)
   
-  	var url="http://localhost:8080/appointment/add"
+  	var finUrl="http://localhost:8080/appointment/add"     	
+      var jsonData={};
+        	jsonData["name"]=name;
+        	jsonData["deptId"]=deptId;
+        	jsonData["email"]=email;
+        	jsonData["mobile"]=mobile;
+        	var today=new Date();
+        	jsonData["time"]=today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        	jsonData["date"]=today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+        	console.log(jsonData);
+        	var xhr = new XMLHttpRequest();
+			xhr.open("POST",finUrl,true);
+			xhr.setRequestHeader("Content-Type", "application/json");
+			xhr.send(JSON.stringify(jsonData));
+			
+			xhr.onreadystatechange = processRequest;
+			function processRequest(e) {
+				if (xhr.readyState == 4) {
+					console.log("Appointment Made");
+					alert("Appointment booked");
+				}
+			}
+			
  
-  function processRequest(e) {
-    if (reqForRoom.readyState == 4) {
-        // time to partay!!!
-        console.log(reqForRoom.response);
-      }
-  }
-  if(reqForRoom.readyState === XMLHttpRequest.DONE){
-    var data = JSON.parse(reqForRoom.response);
-      console.log(data);
-      var rooMenu=JSON.parse(data.roomMenu);
-      console.log(roomMenu)
   }
   
-    
-
-  /*var finUrl='http://localhost:8080/rooms/update';
-  var json="{"roomId":,"bedId":"", "roomType":"","patientId":patientId,"availability":"No"}";
-  var xhr = new XMLHttpRequest();
-  xhr.open("POST",finUrl,true);
-  xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.send(json);
-  req.open('POST', finUrl, true);
-    if (xhr.status === 201) {
-      const data = JSON.parse(req.responseText);
-      let opt1;
-      for (let i = 0; i < data.length; i++) {
-          opt1 = document.createElement('option');
-          console.log(data[i])
-          opt1.text = data[i].firstName+' '+data[i].lastName;
-          opt1.value=data[i].patientId;
-        pat.add(opt1);
-     
-      }
-   } else {
-    // Reached the server, but it returned an error
-  }   */
-}
