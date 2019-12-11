fbfb8e6db662eada33e84957a14ec835b24b7a67
Mudit Garg <mudgarg@Mudits-MacBook-Pro.local>
2019-12-11 04:49:18 -0500

    encryption

---
+++ doctorNameId.js
@@ -0,0 +1,37 @@
+var docid = document.getElementById('doctor-down');
+console.log(docid)
+docid.length = 0;
+
+var doctext = document.createElement('option');
+doctext.text = 'Select Doctor';
+
+docid.add(doctext);
+docid.selectedIndex = 0;
+
+const docurl = 'http://localhost:8080/doctors/all';
+
+const docreq = new XMLHttpRequest();
+docreq.open('GET', docurl, true);
+
+docreq.onload = function() {
+  if (docreq.status === 200) {
+    const docdata = JSON.parse(docreq.responseText);
+    let docopt;
+    for (let i = 0; i < docdata.length; i++) {
+      docopt = document.createElement('option');
+      console.log(docdata[i])
+      docopt.text = docdata[i].name;
+      docopt.value=docdata[i].doctor_id;
+	  docid.add(docopt);
+     
+    }
+   } else {
+    // Reached the server, but it returned an error
+  }   
+}
+
+docreq.onerror = function() {
+  console.error('An error occurred fetching the JSON from ' + paturl);
+};
+
+docreq.send(); 
